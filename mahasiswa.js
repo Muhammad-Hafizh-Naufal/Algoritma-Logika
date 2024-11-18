4b05141d435c4990846ea9f9030f67f101ec9bf5
@Hafizh_Naufal <noval1122567@gmail.com>
2024-11-19 00:02:30 +0700

    Looping Data

---
+++ mahasiswa.js
@@ -0,0 +1,38 @@
+let mahasiswa = [
+  {
+    id: 1,
+    npm: "210001",
+    name: "John Doe",
+    class: "TI-1A",
+  },
+  {
+    id: 2,
+    npm: "210002",
+    name: "Timmy Doe",
+    class: "T3-1A",
+  },
+  {
+    id: 3,
+    npm: "123456",
+    name: "Jail",
+    class: "TI-1A",
+  },
+  {
+    id: 4,
+    npm: "6543321",
+    name: "Tintin",
+    class: "KA-1A",
+  },
+  {
+    id: 5,
+    npm: "010203",
+    name: "Joseph Stalin",
+    class: "BE-1A",
+  },
+];
+
+// ngambil nama mahasiswa dari array
+
+mahasiswa.map((mhs) => {
+  console.log(mhs.name);
+});
