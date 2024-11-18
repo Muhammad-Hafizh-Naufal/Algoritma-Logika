91e2a996a213abf7d1a4e8aeea6465ad963f62f1
@Hafizh_Naufal <noval1122567@gmail.com>
2024-11-18 23:18:11 +0700

    CekGanjilGenap

---
+++ cekGanjilGenap.js
@@ -0,0 +1,8 @@
+let input = prompt("Masukkan angka: ");
+console.log(input);
+
+if (input % 2 === 0) {
+  console.log(`${input} adalah bilangan genap`);
+} else {
+  console.log(`${input} adalah bilangan ganjil`);
+}
