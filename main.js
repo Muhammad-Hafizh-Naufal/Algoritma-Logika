743bd3751d5331186b087d1e087337738484752d
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-17 14:59:22 +0700

    countsheep

--- main.js
+++ main.js
@@ -2,22 +2,16 @@ console.log("Belajar JavaScript Dasar");
 console.log("========================");
 
 // start
-let nilai = [80, 65, 90, 50, 75, 88, 92, 60];
 
-let tidakLulus = nilai.filter((n) => n < 75);
-let lulus = nilai.filter((n) => n >= 75);
+var countSheep = function (num) {
+  let gabungkan = "";
+  if (num === 0) return "";
 
-let total = nilai.reduce((acc, n) => acc + n, 0);
-let rata = total / nilai.length;
+  for (let i = 1; i <= num; i++) {
+    gabungkan += `${i} sheep...`;
+  }
 
-let kategori =
-  rata >= 85
-    ? "Kelas Sangat Baik"
-    : rata >= 75
-      ? "Kelas Baik"
-      : "Kelas Perlu Evaluasi";
+  return gabungkan;
+};
 
-console.log("Tidak Lulus:", tidakLulus);
-console.log("Jumlah Lulus:", lulus.length);
-console.log("Rata-rata:", rata);
-console.log("Kategori:", kategori);
+console.log(countSheep(3));
