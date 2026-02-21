1b398a420e31bc4eab20798be1929f527dedb6b0
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-21 15:57:21 +0700

    setter & getter

--- oop.js
+++ oop.js
@@ -1,37 +1,24 @@
-class Mahasiswa {
-  prodi = "FIKTI";
-  constructor(nama, nilai) {
-    console.log(`inislisasi data ${nama}`);
+console.log("====================Hewan=====================");
+
+class Hewan {
+  constructor(nama) {
     this.nama = nama;
-    this.nilai = nilai;
-  }
-  halo() {
-    console.log(`hallo nama saya ${this.nama} dari prodi ${this.prodi}`);
   }
 
-  lulus() {
-    if (this.nilai >= 75) {
-      console.log(`${this._nama} Lulus dengan nilai ${this.nilai}`);
-    } else {
-      console.log(`${this._nama} Tidak Lulus dengan nilai ${this.nilai}`);
-    }
-  }
-
-  get nama() {
-    return this._nama.toUpperCase();
-  }
-
-  set nama(value) {
-    if (value.length < 3) {
-      console.log("nama anda kurang panjang");
-      return;
-    }
-    this._nama = value;
+  jalan() {
+    console.log(`${this.nama} sedang berjalan`);
   }
 }
 
-const mhs1 = new Mahasiswa("oca", 90);
-const mhs2 = new Mahasiswa("ara", 70);
+class Burung extends Hewan {
+  terbang() {
+    console.log(`${this.nama} sedang terbang`);
+  }
+}
 
-mhs2.nama = "zahraa";
-mhs2.halo();
+let kucing = new Hewan("Kucing oyen");
+kucing.jalan();
+
+let pipit = new Burung("Burung Pipit");
+pipit.jalan();
+pipit.terbang();
