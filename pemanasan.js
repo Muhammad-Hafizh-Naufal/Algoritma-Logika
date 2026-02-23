dd9cd4d9ec2032d978752daf079cebe3a1e7e3f2
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-23 19:36:11 +0700

    rata-rata & terbaik

--- pemanasan.js
+++ pemanasan.js
@@ -1,27 +1,21 @@
-// Total Belanja (Studi Kasus Keranjang)
-let cart = [
-  { nama: "Laptop", harga: 7000000, qty: 1 },
-  { nama: "Mouse", harga: 200000, qty: 2 },
-  { nama: "Keyboard", harga: 500000, qty: 1 },
+let data = [
+  { nama: "Andi", nilai: [80, 75, 90] },
+  { nama: "Budi", nilai: [60, 70, 65] },
+  { nama: "Citra", nilai: [95, 85, 100] },
 ];
 
-let kalkulasi = cart.map((items) => {
-  let totalbrg = items.qty * items.harga;
+let rata = data.map((siswa) => {
+  let total = siswa.nilai.reduce((t, n) => t + n, 0) / siswa.nilai.length;
 
   return {
-    nama: items.nama,
-    qty: items.qty,
-    totalbrg: totalbrg,
+    nama: siswa.nama,
+    ratarata: Math.ceil(total),
   };
 });
+console.log(rata);
 
-let totalharga = kalkulasi.reduce((total, b) => total + b.totalbrg, 0);
+let nilaiTerbesar = rata.reduce((max, siswa) => {
+  return siswa.ratarata > max.ratarata ? siswa : max;
+});
 
-let hargafinal = 0;
-if (totalharga > 7000000) {
-  hargafinal = totalharga - totalharga * 0.1;
-}
-
-console.log(kalkulasi);
-console.log(`Total Harga ${totalharga}`);
-console.log(`harga final : ${hargafinal}`);
+console.log(nilaiTerbesar);
