57b65a386ad7b9c9c797e1d07df69d3e5bb61db3
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-23 20:21:46 +0700

    Sistem Kelulusan (Mini Studi Kasus)

--- pemanasan.js
+++ pemanasan.js
@@ -1,14 +1,25 @@
-function hitunghuruf(kalimat) {
-  let hasil = {};
-  for (let huruf of kalimat) {
-    if (hasil[huruf]) {
-      hasil[huruf] += 1;
-    } else {
-      hasil[huruf] = 1;
-    }
+let mahasiswa = [
+  { nama: "Andi", nilai: 85 },
+  { nama: "Budi", nilai: 60 },
+  { nama: "Citra", nilai: 75 },
+];
+
+let penilaian = mahasiswa.map((mhs) => {
+  let hasil = "";
+
+  if (mhs.nilai >= 75) {
+    hasil = "lulus";
+  } else if (mhs.nilai >= 60) {
+    hasil = "remedial";
+  } else {
+    hasil = "tidak lulus";
   }
 
-  return hasil;
-}
+  return {
+    nama: mhs.nama,
+    nilai: mhs.nilai,
+    status: hasil,
+  };
+});
 
-console.log(hitunghuruf("jawa"));
+console.log(penilaian);
