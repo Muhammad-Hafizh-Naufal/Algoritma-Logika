9c7b5b3cb7d8e35f60517e4f8725212732916a3e
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-24 12:00:38 +0700

    Update stok setelah transaksi.
    Tentukan barang dengan stok di bawah 5 (stok menipis).
    
    Tentukan total seluruh stok tersisa.
    
    Tambahkan barang baru: Printer stok 7.
    
    Hapus barang yang stoknya 0.

--- pemanasan.js
+++ pemanasan.js
@@ -1,39 +1,32 @@
 let data = [
-  {
-    name: "Produk A",
-    harga: 100000,
-    qty: 2,
-  },
-  {
-    name: "Produk B",
-    harga: 50000,
-    qty: 1,
-  },
-  {
-    name: "Produk C",
-    harga: 200000,
-    qty: 3,
-  },
+  { nama: "laptop", stok: 5 },
+  { nama: "mouse", stok: 20 },
+  { nama: "keyboard", stok: 15 },
+  { nama: "monitor", stok: 3 },
+  { nama: "usb hub", stok: 0 },
+  { nama: "type c", stok: 0 },
 ];
 
-let subtotal = data.map((items) => {
-  return {
-    ...items,
-    total: items.harga * items.qty,
-  };
-});
+let transaksi = [
+  { nama: "laptop", stok: 2 },
+  { nama: "mouse", stok: 5 },
+  { nama: "monitor", stok: 1 },
+];
 
-let totalbelanja = subtotal.reduce((total, b) => total + b.total, 0);
-let barangtermahal = [...subtotal]
-  .sort((a, b) => b.total - a.total)
-  .splice(0, 1);
+let newstock = data
+  .map((item) => {
+    let transaksiItem = transaksi.find((t) => t.nama === item.nama);
+    return {
+      ...item,
+      stok: item.stok - (transaksiItem ? transaksiItem.stok : 0),
+    };
+  })
+  .filter((item) => item.stok > 0);
 
-let diskon = 0;
-if (totalbelanja > 500000) {
-  diskon = totalbelanja - totalbelanja * 0.1;
-}
+let cekstock = newstock.filter((items) => items.stok < 5);
+let totalstok = newstock.reduce((total, s) => total + s.stok, 0);
+let newitem = newstock.push({ nama: "printer", stok: 7 });
 
-console.log(subtotal);
-console.log(`total harga barang: ${totalbelanja}`);
-console.log("subtotal termahal:", barangtermahal);
-console.log(`total harga setelah diskon: ${diskon}`);
+console.log("update stok;", newstock);
+console.log("stok dibawah 5:", cekstock);
+console.log("total stok: ", totalstok);
