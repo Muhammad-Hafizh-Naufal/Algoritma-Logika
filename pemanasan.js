d5c6ed9609f18cb5176f88d81f34e42a666fb87f
@Hafizh_Naufal <noval1122567@gmail.com>
2026-02-24 10:36:40 +0700

    Studi Kasus 1: Sistem Keranjang Belanja (E-Commerce)

--- pemanasan.js
+++ pemanasan.js
@@ -1,16 +1,39 @@
-let nilai = [
-  { nama: "Andi", skor: 85 },
-  { nama: "Budi", skor: 92 },
-  { nama: "Citra", skor: 88 },
-  { nama: "dita", skor: 88 },
+let data = [
+  {
+    name: "Produk A",
+    harga: 100000,
+    qty: 2,
+  },
+  {
+    name: "Produk B",
+    harga: 50000,
+    qty: 1,
+  },
+  {
+    name: "Produk C",
+    harga: 200000,
+    qty: 3,
+  },
 ];
 
-let urutkan = [...nilai]
-  .sort((max, n) => n.skor - max.skor)
-  .map((siswa, index) => {
-    return {
-      ...siswa,
-      ranking: index + 1,
-    };
-  });
-console.log(urutkan);
+let subtotal = data.map((items) => {
+  return {
+    ...items,
+    total: items.harga * items.qty,
+  };
+});
+
+let totalbelanja = subtotal.reduce((total, b) => total + b.total, 0);
+let barangtermahal = [...subtotal]
+  .sort((a, b) => b.total - a.total)
+  .splice(0, 1);
+
+let diskon = 0;
+if (totalbelanja > 500000) {
+  diskon = totalbelanja - totalbelanja * 0.1;
+}
+
+console.log(subtotal);
+console.log(`total harga barang: ${totalbelanja}`);
+console.log("subtotal termahal:", barangtermahal);
+console.log(`total harga setelah diskon: ${diskon}`);
